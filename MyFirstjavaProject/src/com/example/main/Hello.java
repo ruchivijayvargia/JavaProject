fd3844eb5cd83d38e2c1cf2989ce683f3a16fa3a
rvijayvargia <ruchi.vijayvargi@gmail.com>
2016-04-19 11:58:11 -0500

    Change1

---
+++ MyFirstjavaProject/src/com/example/main/Hello.java
@@ -0,0 +1,16 @@
+package com.example.main;
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Hi There !!");
+		System.out.println("Integrate java project with Git..");
+	
+		System.out.println("First Git Commit line");
+		
+		System.out.println("First Git Commit line");
+		System.out.println("First Git Commit line");
+		
+		
+	}
+}
---
+++ MyFirstjavaProject/src/com/example/util/StringUtil.java
@@ -0,0 +1,13 @@
+package com.example.util;
+
+public class StringUtil {
+	public String convertLower(String str){
+		return str.toLowerCase();
+	}
+	
+	public String convertUpper(String str){
+		return str.toUpperCase();
+	}
+	
+	
+}
